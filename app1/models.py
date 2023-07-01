e2e4b97ca39d3bea03e22a280c87e1a0791383a3
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-07-01 10:57:09 +0200

    2. Tabelle

--- app1/models.py
+++ app1/models.py
@@ -21,3 +21,23 @@ class Eintrag(models.Model):
 
     def get_absolute_url(self):
         return reverse("Eintrag_detail", kwargs={"pk": self.pk})
+
+class Kommentar(models.Model):
+    eintrag = models.ForeignKey(Eintrag, verbose_name="Blog-Eintrag", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="Verfasser", on_delete=models.CASCADE)
+    bewertung = models.IntegerField("Bewertung")
+    slug = models.SlugField("Schlagwort")
+    kommentar = models.TextField("Kommentar")
+    erstellt = models.DateTimeField("Erstellt", auto_now=False, auto_now_add=True)
+    geaendert = models.DateTimeField("Geändert", auto_now=True, auto_now_add=False)
+    aktiv = models.BooleanField("Aktiv", default=True)
+    class Meta:
+        verbose_name = "Kommentar"
+        verbose_name_plural = "Kommentare"
+        ordering = ['user', '-geaendert']
+
+    def __str__(self):
+        return f"{self.user} - {self.geaendert} / {self.slug}"
+
+    def get_absolute_url(self):
+        return reverse("Kommentar_detail", kwargs={"pk": self.pk})
