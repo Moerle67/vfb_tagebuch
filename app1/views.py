91fb6723364dce06c2e0f4f4db84df2dd59e4a2b
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-07-01 11:52:26 +0200

    Templates und Liste

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def hello(request):
-    return 
+    inhalt = Eintrag.objects.filter(aktiv=True)
+    content = {
+        'inhalt': inhalt,
+        'ueber': "Noch eine neue Überschrift"
+    }
+    return render(request, 'app1/liste_eintraege.html', content)
