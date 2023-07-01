fe3df0623f937892a07ed5737fe37fc1ab0ceb18
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-07-01 10:15:32 +0200

    Modell1 Eintrag

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Eintrag(models.Model):
+    author = models.ForeignKey(User, verbose_name=("Verfasser"), on_delete=models.CASCADE)
+    ueberschrift = models.CharField("Überschrift", max_length=50)
+    slug = models.SlugField("Schlagwort")
+    text = models.TextField("Eintrag")
+    erstellt = models.DateTimeField("Erstellt", auto_now=False, auto_now_add=True)
+    geaendert = models.DateTimeField("Geändert", auto_now=True, auto_now_add=False)
+    aktiv = models.BooleanField("Aktiv", default=True)
+    class Meta:
+        verbose_name = "Eintrag"
+        verbose_name_plural = "Einträge"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Eintrag_detail", kwargs={"pk": self.pk})
